29cccf12fde0577a4277d2f83804d4097ad146ed
hvh <ext.hien.v.huynh@dektech.com.au>
2021-12-09 00:11:39 +0700

    server can receive file posted

--- server/index.js
+++ server/index.js
@@ -3,27 +3,13 @@ const fs = require("fs")
 const express = require("express")
 const route = express.Router()
 const server = express()
+const formidable = require("formidable")
 
-//server.use(bodyParser());
-
-
-/*const server = http.createServer((req, res) => {
-
-    //console.log(req.url)
-    if (req.url=="/") {
-        handleWelcome(req, res)
-    } else if (req.url=='/upload') {
-        handleUpload(req, res)
-    } else if (req.url=='/trans') {
-        handleGetTrans(req, res)
-    }
-    
-})*/
 
 server.use(express.json())
 server.use(express.urlencoded({
     extended: true
-  }))
+}))
 
 route.get("/", (req, res) => {
     handleWelcome(req, res)
@@ -56,20 +42,13 @@ function handleUpload(req, res) {
     res.writeHead(200, {
             "Context-type":"json/plain"
     })
-    //req.pipe(req.busyboy)
-    console.log(req.body)
-    /*req.busyboy.on('file', (fieldname, file, filename) => {
-        var buf = '';
-        file.on('data', function(d) {
-            buf += d;
-        }).on('end', function() {
-            var val = JSON.parse(buf);
-            // use `val` here ...
-        }).setEncoding('utf8');
-    })*/ 
-    console.log(req.body.data)
-    console.log(req.query)
-    console.log(Object.keys(req))
+    
+    //console.log(Object.keys(req))
+    const form = new formidable.IncomingForm()
+    form.parse(req, (err, fields, files) => {
+        const thefile = files.file
+        console.log(thefile)
+    })
     
     res.write("posted!\n")
     res.end()
